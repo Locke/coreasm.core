66e9c3f803747daf3ddec793c5e6f8153ec40e1d
André Wolski <locke@dena-design.de>
2022-10-07 20:46:41 +0200

    Engine: warn if commandQueue is not empty upon termination

--- org.coreasm.engine/src/org/coreasm/engine/Engine.java
+++ org.coreasm.engine/src/org/coreasm/engine/Engine.java
@@ -1058,8 +1058,11 @@ public class Engine implements ControlAPI {
 
 			isBusyLock.lock();
 			try {
-				// empty command queue
-				commandQueue.clear();
+				// empty command queue and warn if any commands are left
+				EngineCommand cmd;
+				while ((cmd = commandQueue.poll()) != null) {
+					logger.warn("Ignore user command {}, Engine is terminated", cmd.type);
+				}
 
 				engineBusy = false;
 				isNotBusy.signalAll();
