Locke/coreasm.core

commit 667a969cf2ef180999cca9f9695a60c756ddd7d6

eclipse: align module names and versions

org.coreasm.eclipse.parent/pom.xml
@@ -4,15 +4,13 @@
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
 	<modelVersion>4.0.0</modelVersion>
 
-	<artifactId>org.coreasm.eclipse.parent</artifactId>
+	<artifactId>coreasm-eclipse-parent</artifactId>
 	<name>CoreASM Eclipse Parent Project</name>
-	<version>1.1.1-SNAPSHOT</version>
 
 	<parent>
-		<artifactId>org.coreasm.parent</artifactId>
-		<groupId>org.coreasm</groupId>
-		<version>1.7.3-SNAPSHOT</version>
-		<relativePath>../org.coreasm.parent</relativePath>
+		<groupId>de.athalis.coreasm</groupId>
+		<artifactId>coreasm-parent</artifactId>
+		<version>${revision}</version>
 	</parent>
 
 	<packaging>pom</packaging>

org.coreasm.eclipse/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Eclipse-BundleShape: dir
 Bundle-ManifestVersion: 2
 Bundle-Name: CoreASM Eclipse Plugin
-Bundle-SymbolicName: org.coreasm.eclipse;singleton:=true
+Bundle-SymbolicName: coreasm-eclipse;singleton:=true
 Bundle-Activator: org.coreasm.eclipse.CoreASMPlugin
 Bundle-Vendor: coreasm.org
 Require-Bundle: org.eclipse.ui,

org.coreasm.eclipse.feature/pom.xml
@@ -4,13 +4,13 @@
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
 	<modelVersion>4.0.0</modelVersion>
 
-	<artifactId>org.coreasm.eclipse.feature</artifactId>
+	<artifactId>coreasm-eclipse-feature</artifactId>
 	<name>CoreASM Eclipse Plugin Feature</name>
 
 	<parent>
-		<version>1.1.1-SNAPSHOT</version>
-		<groupId>org.coreasm</groupId>
-		<artifactId>org.coreasm.eclipse.parent</artifactId>
+		<groupId>de.athalis.coreasm</groupId>
+		<artifactId>coreasm-eclipse-parent</artifactId>
+		<version>${revision}</version>
 		<relativePath>../org.coreasm.eclipse.parent</relativePath>
 	</parent>
 

org.coreasm.eclipse.repository/pom.xml
@@ -4,13 +4,13 @@
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
 	<modelVersion>4.0.0</modelVersion>
 
-	<artifactId>org.coreasm.eclipse.repository</artifactId>
+	<artifactId>coreasm-eclipse-repository</artifactId>
 	<name>CoreASM Eclipse Plugin Repository</name>
 
 	<parent>
-		<version>1.1.1-SNAPSHOT</version>
-		<groupId>org.coreasm</groupId>
-		<artifactId>org.coreasm.eclipse.parent</artifactId>
+		<groupId>de.athalis.coreasm</groupId>
+		<artifactId>coreasm-eclipse-parent</artifactId>
+		<version>${revision}</version>
 		<relativePath>../org.coreasm.eclipse.parent</relativePath>
 	</parent>
 

org.coreasm.eclipse/pom.xml
@@ -4,13 +4,13 @@
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
 	<modelVersion>4.0.0</modelVersion>
 
-	<artifactId>org.coreasm.eclipse</artifactId>
+	<artifactId>coreasm-eclipse</artifactId>
 	<name>CoreASM Eclipse Plugin</name>
 
 	<parent>
-		<artifactId>org.coreasm.eclipse.parent</artifactId>
-		<groupId>org.coreasm</groupId>
-		<version>1.1.1-SNAPSHOT</version>
+		<groupId>de.athalis.coreasm</groupId>
+		<artifactId>coreasm-eclipse-parent</artifactId>
+		<version>${revision}</version>
 		<relativePath>../org.coreasm.eclipse.parent</relativePath>
 	</parent>
 
@@ -18,9 +18,9 @@
 
 	<dependencies>
 		<dependency>
-			<groupId>org.coreasm</groupId>
-			<artifactId>org.coreasm.engine</artifactId>
-			<version>1.7.3-SNAPSHOT</version>
+			<groupId>de.athalis.coreasm</groupId>
+			<artifactId>coreasm-engine</artifactId>
+			<version>${revision}</version>
 		</dependency>
 	</dependencies>
 

commit 76a12eedd1aebd8e63bb8aa79f70059ea29cc82f

eclipse: complete aligning module names
- update GroupId from upstream `org.coreasm` to forked `de.athalis.coreasm`
- update remaining ArtifactId references from `org.coreasm.engine` to `coreasm-engine`

Should have been part of original commit.

Fixes: 667a969cf2ef180999cca9f9695a60c756ddd7d6 ("eclipse: align module names and versions")

org.coreasm.eclipse.parent/pom.xml
@@ -56,7 +56,7 @@
 						<configuration>
 							<excludeTransitive>true</excludeTransitive>
 							<excludeGroupIds>*</excludeGroupIds>
-							<includeGroupIds>org.coreasm</includeGroupIds>
+							<includeGroupIds>de.athalis.coreasm</includeGroupIds>
 							<stripVersion>true</stripVersion>
 							<outputDirectory>${project.build.directory}/lib/</outputDirectory>
 						</configuration>

org.coreasm.eclipse/META-INF/MANIFEST.MF
@@ -15,7 +15,7 @@ Require-Bundle: org.eclipse.ui,
  org.eclipse.jdt.ui,
  org.eclipse.ui.navigator
 Bundle-RequiredExecutionEnvironment: JavaSE-17
-Bundle-ClassPath: ., target/lib/org.coreasm.engine.jar
+Bundle-ClassPath: ., target/lib/coreasm-engine.jar
 Import-Package: org.eclipse.ui.dialogs,
  org.eclipse.ui.texteditor.templates,
  org.eclipse.ui.views.contentoutline

org.coreasm.eclipse/src/org/coreasm/eclipse/CoreASMPlugin.java
@@ -60,8 +60,7 @@ public class CoreASMPlugin extends AbstractUIPlugin {
 		Tools.setRootFolder(rootFolder);
 		File folder = new File(new File(new File(rootFolder), "target"), "lib");
 
-		System.setProperty(Tools.COREASM_ENGINE_LIB_PATH, new File(folder, "org.coreasm.engine.jar").getAbsolutePath());
-
+		System.setProperty(Tools.COREASM_ENGINE_LIB_PATH, new File(folder, "coreasm-engine.jar").getAbsolutePath());
 	}
 
 	// TODO clean up the following two methods